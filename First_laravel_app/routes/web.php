d27c452b6884cb8bed3bed1e2c530c9deeb497a2
ADITYA JHA <a4adityajha@gmail.com>
2024-01-17 19:32:38 +0530

    added DemoController(basic controller)

--- First_laravel_app/routes/web.php
+++ First_laravel_app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route ::get("/" , [DemoController::class , "index"]);
 
 Route::get('/hello',function(){
     return view('hello');
